ae89cf587a4516d54d248778558ec25f92081f3e
Avi1764 <avinashpaleti5@gmail.com>
2026-09-05 14:59:22 +0530

    Modules and modular programming with ATM and Calculator projects

---
+++ Day-24/cal/main.py
@@ -0,0 +1,43 @@
+'''
+import logic
+
+logic.add(2,3)
+logic.sub(6,7)
+logic.mul(5,8)
+logic.div(7,4)
+logic.exp(9,5)
+logic.rem(7,5)
+
+'''
+
+
+'''
+import logic as lg
+
+lg.add(2,3)
+lg.sub(6,7)
+lg.mul(5,8)
+lg.div(7,4)
+lg.exp(9,5)
+lg.rem(7,5)
+
+'''
+
+
+'''
+from logic import add,sub
+
+add(2,3)
+sub(9,5)
+
+'''
+
+
+from logic import *
+
+add(2,3)
+sub(6,7)
+mul(5,8)
+div(7,4)
+exp(9,5)
+rem(7,5)
