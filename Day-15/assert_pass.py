6e29b398c05e83975bef131a264993a366b56708
Avi1764 <avinashpaleti5@gmail.com>
2026-09-05 14:59:20 +0530

    Assert, pass statements, and nested loop patterns

---
+++ Day-15/assert_pass.py
@@ -0,0 +1,8 @@
+
+# pass is to define empty block of code.
+
+'''
+if 10%2==0:
+    pass
+'''
+
---
+++ Day-15/patterns.py
@@ -0,0 +1,55 @@
+'''
+
+for i in range(5):
+    for j in range(5):
+        print('*',end=' ')
+    print()
+'''
+
+'''
+for row in range(5):
+    for col in range(5):
+        print(row,end=' ')
+    print()
+'''
+
+'''
+for row in range(5):
+    for col in range(5):
+        print(col,end=' ')
+    print()
+'''
+
+'''
+for i in range(5):
+    for j in range(5):
+        print(i+j,end=' ')
+    print()
+
+'''
+
+'''
+for i in range(5):
+    for j in range(5):
+        if j%2==0:
+            print('0',end=" ")
+        else:
+             print('1',end=" ")
+    print()
+'''
+
+'''
+for i in range(5):
+    for j in range(5):
+        print(j%2,end=' ')
+    print()
+'''
+
+
+for i in range(5):
+    for j in range(5-i):
+            print('A',end=" ")
+    print()
+
+
+
